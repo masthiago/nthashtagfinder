4b1894d3d74bff3c3605c03b550035a954244b36
Paulo Ricardo <paulodev07@gmail.com>
2023-05-03 17:13:37 -0300

    Main Commit inicial da pagina Home

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,6 +1,33 @@
+import {
+  StyledHeader,
+  StyledButton,
+  StyledMain,
+} from './components/Home.style';
+import iconAbout from './assets/iconAbout.svg';
+import iconUser from './assets/iconUser.svg';
+
 function App() {
   return (
-    <>Hello world. Hashtag Finder.</>
+    <>
+      <StyledHeader>
+        <div>
+          hashtag<strong>finder</strong>
+        </div>
+        <div>
+          <StyledButton backgroundColor='#72EFDB' color='#0a1744'>
+            <img className='iconHeader' src={iconAbout} alt='ícone Sobre' />
+            Sobre
+          </StyledButton>
+          <StyledButton>
+            <img className='iconHeader' src={iconUser} alt='ícone Login' />
+            Login
+          </StyledButton>
+        </div>
+      </StyledHeader>
+      <StyledMain>
+        <></>
+      </StyledMain>
+    </>
   );
 }
 
