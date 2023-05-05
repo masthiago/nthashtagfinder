78fcb10f985a4528229fbe090e5b08e12d71cb97
Thiago Medeiros <thiago@tuta.io>
2023-05-05 04:09:31 -0300

    results area and minor adjusts

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -9,7 +9,7 @@ import {
   ResultsImageContainer,
   ResultsPostContainer,
   Tab,
-} from "./styled";
+} from "./styled"; // Styled components
 
 export default class Results extends React.Component {
   constructor(props) {
@@ -30,53 +30,112 @@ export default class Results extends React.Component {
     window.addEventListener("resize", null);
   }
 
-  handleResize = (size, event) => {
+  handleResize = () => {
+    // Update the state with the new window size
     this.setState({ size: window.innerWidth });
   };
 
-  DisplayWithTabs = () => {
+  displayWithTabs = () => {
+    // Use the TabGroup component to display the results
+    // TODO Revise this function and TabGroup to reduce code
     return this.TabGroup();
   };
 
-  DisplayWithoutTabs = () => {
-    return <Fragment>DisplayWithoutTabs</Fragment>;
+  displayWithoutTabs = () => {
+    const TwitterImages = this.TwitterImages;
+    const TwitterPosts = this.TwitterPosts;
+    return (
+      <>
+        <TwitterImages />
+        <TwitterPosts />
+      </>
+    );
   };
 
   TwitterImages = () => {
+    // Random image generator
+    // This will be removed when API is implemented
+    const ImgGen = () => {
+      let tags = [];
+      for (let i = 0; i < 10; i++) {
+        tags.push(
+          <ResultImage
+            key={"image" + (i + 1)}
+            background={`https://loremflickr.com/720/720/`}
+          >
+            <p>
+              <small>Publicado por:</small>
+            </p>
+            <p>
+              <b>@twitterusername</b>
+            </p>
+          </ResultImage>
+        );
+      }
+      return tags;
+    };
+
     return (
       <ResultsImageContainer>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
-        <ResultImage>&nbsp;</ResultImage>
+        <ImgGen />
       </ResultsImageContainer>
     );
   };
 
   TwitterPosts = () => {
+    // Random post generator
+    // This will be removed when API is implemented
+    const PostGen = () => {
+      let tags = [];
+      for (let i = 0; i < 10; i++) {
+        tags.push(
+          <ResultPost key={"post" + (i + 1)}>
+            <div>
+              <img
+                alt=""
+                src="https://loremflickr.com/360/360/face/"
+                style={{
+                  width: "64px",
+                  height: "64px",
+                  borderRadius: "100%",
+                }}
+              />
+            </div>
+            <div>
+              <p>
+                <strong>UserName </strong>
+                <small> @username</small>
+              </p>
+              <p>
+                Laborum ut nostrud voluptate reprehenderit id ullamco nisi non
+                aliquip ad et.
+              </p>
+              <br />
+              <p
+                style={{
+                  color: "#72efdb",
+                  fontWeight: "bold",
+                }}
+              >
+                Ver mais no twitter
+              </p>
+            </div>
+          </ResultPost>
+        );
+      }
+      return tags;
+    };
+
     return (
       <ResultsPostContainer>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
-        <ResultPost>&nbsp;</ResultPost>
+        <PostGen />
       </ResultsPostContainer>
     );
   };
 
   TabGroup = () => {
+    // Check which tab is active and displays the corresponding
+    // content according to the {state.active}
     return (
       <>
         <ButtonGroup>
@@ -99,8 +158,12 @@ export default class Results extends React.Component {
   };
 
   render() {
-    const DisplayWithTabs = this.DisplayWithTabs;
-    const DisplayWithoutTabs = this.DisplayWithoutTabs;
+    // The methods this.displayWithTabs and this.displayWithoutTabs return React components,
+    // but to be called they need to start with capital letters, so here they
+    // are referenced as the constants DisplayWithTabs and DisplayWithoutTabs for that purpose.
+    const DisplayWithTabs = this.displayWithTabs;
+    const DisplayWithoutTabs = this.displayWithoutTabs;
+
     return (
       <ResultsContainer>
         <ResultsHeader>
@@ -108,6 +171,10 @@ export default class Results extends React.Component {
         </ResultsHeader>
 
         <ResultsDynamicContainer>
+          {/* 
+            Alternates between display forms with or without tabs 
+            depending on the size of the screen. 
+          */}
           {this.state.size > 1024 ? (
             <DisplayWithoutTabs />
           ) : (
