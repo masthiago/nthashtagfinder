fbbd4d958b5243baa0ee3c8b9696bbc18d5655eb
Paulo Ricardo <paulodev07@gmail.com>
2023-05-07 10:22:34 -0300

    Segundo commit, finalização da Home estatica

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { GlobalStyle } from './App/components/Global.style';
+import { GlobalStyle } from './App/homeComponents/globalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
