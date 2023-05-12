7d0f7f055374328b14a8f2231f5d7f8f5174a3e8
Gabriel97Project <gabriel97tomaz@hotmail.com>
2023-05-12 16:57:28 -0300

    iniciando processo de request na pagina about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AboutMainStyled, AboutTextAndImageStyle, AboutUsStyle, } from "./styled";
 import Header from "../Header";
 import Footer from "../Footer";
@@ -7,26 +7,80 @@ import emailBoxIcon from "../../assets/img/envelope_font_awesome.svg";
 import gitHubIcon from "../../assets/img/github.svg";
 import linkednIcon from "../../assets/img/linkedin.svg";
 import developerInfos from "./developers.json";
+import Airtable from "airtable";
 
 export default function AboutPageContent() {
   const userData = [...developerInfos];
+
+  const [siteRecordState, setSiteRecordState] = useState([]);
+  useEffect(() => {
+    let records = [];
+    var base = new Airtable({ apiKey: 'keykXHtsEPprqdSBF' }).base('app6wQWfM6eJngkD4');
+    var squadId = '05-23';
+
+    base('Projeto')
+      .select({
+        filterByFormula: `Squad = '${squadId}'`,
+        view: 'Grid view'
+      })
+      .eachPage(
+        function page(pageRecords, fetchNextPage) {
+           records.push(...pageRecords);
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          const atualizedResults = records.map(function (record) {
+            console.log('Retrieved', record.get('Sobre'));
+            return (record.get('Sobre'));
+          });
+          setSiteRecordState(atualizedResults);
+        }
+
+      );
+    console.log(siteRecordState)
+  }, [])
+
+  /*
+  
+        var Airtable = require('airtable');
+        var base = new Airtable({apiKey: 'keykXHtsEPprqdSBF'}).base('app6wQWfM6eJngkD4');
+        
+        base('Projeto').select({
+            // Selecting the first 3 records in Grid view:
+            
+            view: "Grid view"
+        }).eachPage(function page(records, fetchNextPage) {
+            // This function (`page`) will get called for each page of records.
+        
+            records.forEach(function(record) {
+                console.log('Retrieved', record.get('Squad'));
+            });
+        
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            fetchNextPage();
+        
+        }, function done(err) {
+            if (err) { console.error(err); return; }
+        });
+        
+   */
   return (
     <>
+
       <Header />
       <AboutMainStyled>
         <AboutTextAndImageStyle>
           <div id="titleAndTextContent">
-            <h1>Sobre o projeto</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-              aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-              sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-              duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-              dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam.
-
-            </p>
+            <h1>Sobre o projeto</h1>  
+              {siteRecordState.map((siteRecordStateUnit,index)=>{
+                return <p key={index} style={{ wordWrap: 'break-word' }}>{siteRecordStateUnit}</p>
+              })}
           </div>
           <div id="aboutImageContent">
             <img src={aboutContentImage} alt="ilustrative image" />
